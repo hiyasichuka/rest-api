d8cd505880b89e3e9dd6b3b3b5900f9b23f97aed
Kei Takahashi <takahashi.kgs@om.asahi-kasei.co.jp>
2023-08-13 16:22:20 +0900

    Add api

--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
-export class RecipesService {}
+export class RecipesService {
+  constructor(private prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.recipe.findMany();
+  }
+}
